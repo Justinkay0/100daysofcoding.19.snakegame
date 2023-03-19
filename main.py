268c146280026281b84a8f145f32943bb321e07c
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-20 00:15:34 +0800

    Snake game 4 : Create a snake class for cleaner code and OOP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from time import sleep
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,24 +8,13 @@ screen.bgcolor('black')
 screen.title('Da snake game')
 screen.tracer(0)
 
-snake = []
-
-for _ in range(3):
-    s = Turtle(shape='square')
-    s.color('white')
-    s.penup()
-    s.setposition(y=0, x=0 - _ * 20)
-    snake.append(s)
+snake = Snake()
 
 screen.update()
 
 game_state = True
 while game_state:
-    for segments in range(len(snake) - 1, 0, -1):
-        position = snake[segments -1].pos()
-        snake[segments].goto(x=position[0], y=position[1])
-    snake[0].forward(20)
-
+    snake.move()
     screen.update()
     sleep(0.15)
 
---
+++ snake.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_list = []
+        for _ in range(3):
+            s = Turtle(shape='square')
+            s.penup()
+            s.color('white')
+            s.setposition(y=0, x=0 - _ * 20)
+            self.snake_list.append(s)
+
+
+    def move(self):
+        for segments in range(len(self.snake_list) - 1, 0, -1):
+            position = self.snake_list[segments - 1].pos()
+            self.snake_list[segments].goto(x=position[0], y=position[1])
+        self.snake_list[0].forward(20)
