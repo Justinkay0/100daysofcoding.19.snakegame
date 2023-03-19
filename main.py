cfb74c041610d6204b17329a7671417f78a5f6f8
Justin Kay <14783039+Justinkay0@users.noreply.github.com>
2023-03-19 23:47:29 +0800

    Snake game 3 : Moving body with the head of the snake

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ screen.update()
 
 game_state = True
 while game_state:
-    for segments in snake:
-        segments.forward(20)
+    for segments in range(len(snake) - 1, 0, -1):
+        position = snake[segments -1].pos()
+        snake[segments].goto(x=position[0], y=position[1])
+    snake[0].forward(20)
+
     screen.update()
     sleep(0.15)
 
